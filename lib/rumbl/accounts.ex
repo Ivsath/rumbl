8aa9b3827aed376e05062441214dda1b917523f6
Irvin Reyes <reyes.yerp@gmail.com>
2020-05-19 00:05:33 -0500

    Define the accounts context

---
+++ lib/rumbl/accounts.ex
@@ -0,0 +1,25 @@
+defmodule Rumbl.Accounts do
+  @moduledoc """
+  The Accounts context.
+  """
+
+  alias Rumbl.Accounts.User
+
+  def list_users do
+    [
+      %User{id: "1", name: "Pamela", username: "pams"},
+      %User{id: "2", name: "Gabriela", username: "gaby"},
+      %User{id: "3", name: "Sergio", username: "checosele"}
+    ]
+  end
+
+  def get_user(id) do
+    Enum.find(list_users(), fn map -> map.id == id end)
+  end
+
+  def get_user_by(params) do
+    Enum.find(list_users(), fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end)
+  end
+end
