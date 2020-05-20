83eeac2d2f6a7442778d03aed552df681822fd87
Irvin Reyes <reyes.yerp@gmail.com>
2020-05-20 17:38:11 -0500

    Wire up database access to the Accounts context

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -2,24 +2,26 @@ defmodule Rumbl.Accounts do
   @moduledoc """
   The Accounts context.
   """
-
+  alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
   def list_users do
-    [
-      %User{id: "1", name: "Pamela", username: "pams"},
-      %User{id: "2", name: "Gabriela", username: "gaby"},
-      %User{id: "3", name: "Sergio", username: "checosele"}
-    ]
+    Repo.all(User)
   end
 
   def get_user(id) do
-    Enum.find(list_users(), fn map -> map.id == id end)
+    Repo.get(User, id)
+  end
+
+  def get_user!(id) do
+    Repo.get!(User, id)
   end
 
   def get_user_by(params) do
-    Enum.find(list_users(), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end)
+    Repo.get_by(User, params)
+  end
+
+  def get_user_by!(params) do
+    Repo.get_by!(User, params)
   end
 end
