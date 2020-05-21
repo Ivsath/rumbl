01d22129d3ccb40e1675e373e469702209211f2f
Irvin Reyes <reyes.yerp@gmail.com>
2020-05-21 15:28:12 -0500

    Add new and create actions to the user controller

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -18,4 +18,12 @@ defmodule RumblWeb.UserController do
     changeset = Accounts.change_user(%User{})
     render(conn, "new.html", changeset: changeset)
   end
+
+  def create(conn, %{"user" => user_params}) do
+    {:ok, user} = Accounts.create_user(user_params)
+
+    conn
+    |> put_flash(:info, "#{user.name} created!")
+    |> redirect(to: Routes.user_path(conn, :index))
+  end
 end
